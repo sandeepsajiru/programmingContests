4cb2dd08034253a8e10bd5588a4e875b5ff8b5eb
Sandeep <sandeep.sajiru@gmail.com>
2015-12-06 23:42:36 +0530

    First Problem Solved

---
+++ Day-1/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int uva100();
+
+int main()
+{
+    uva100();
+    return 0;
+}
---
+++ Day-1/uva100.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int getCL(int i)
+{
+    int cl = 0;
+    while(true)
+    {
+        cl++;
+        if(i==1)
+            break;
+
+        if(i%2==0)
+            i = i/2;
+        else
+            i = 3*i+1;
+    }
+    return cl;
+}
+
+int uva100()
+{
+    int i, j, k;
+    int ti, tj;
+    int maxCL, cl;
+    while(scanf("%d %d", &i, &j)==2)
+    {
+
+        ti = i;
+        tj = j;
+
+        if(i>j)
+        {
+            i = tj;
+            j = ti;
+        }
+
+        maxCL = 0;
+
+        for(k=i;k<=j;k++)
+        {
+            cl = getCL(k);
+            if(maxCL < cl)
+                maxCL = cl;
+        }
+        printf("%d %d %d\n", ti, tj, maxCL);
+    }
+    return 0;
+}
