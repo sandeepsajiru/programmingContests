a2f0418fa7766ebfdbaba69d909b6a4e3629902e
Sandeep <sandeep.sajiru@gmail.com>
2015-12-18 00:11:31 +0530

    mandated double quotes

--- commit.cmd
+++ commit.cmd
@@ -2,9 +2,12 @@
 IF %1.==. GOTO ERROR_MSG
 
 git add -A
-git commit -m "%*"
+git commit -m %*
 git config --global credential.helper wincred
 git push origin master
+GOTO END
 
 :ERROR_MSG
 Echo BEINGZERO: Please Specify a Commit Msg
+
+:END
