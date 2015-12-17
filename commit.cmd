97bff575cb7fef01fd3dd3374a807e05fb0495af
Sandeep <sandeep.sajiru@gmail.com>
2015-12-18 00:08:33 +0530

    Updated Commit

--- commit.cmd
+++ commit.cmd
@@ -1,3 +1,10 @@
+@echo off
+IF %1.==. GOTO ERROR_MSG
+
 git add -A
 git commit -m %*
+git config --global credential.helper wincred
 git push origin master
+
+:ERROR_MSG
+Echo BEINGZERO: Please Specify a Commit Msg
